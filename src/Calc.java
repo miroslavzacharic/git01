4e5771163c7a6b5325c106b9f80acc3d40c29328
Miroslav Zacharic <miroslav.zacharic@gmail.com>
2020-04-13 21:11:14 +0300

    sukurti kintamieji

---
+++ src/Calc.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) throws IOException {
+        double x;
+        double y;
+        double sum=0;
+
+  //6      String operation ="";
+
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: " );
+        x = scanner.nextInt();
+        System.out.print("Enter first number: " );
+        y = scanner.nextInt();
+
+        InputStreamReader r=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(r);
+        System.out.print("chose what do you want todo: + , - , * , /");
+
+        String operation=br.readLine();
+
+
+    }
+}
